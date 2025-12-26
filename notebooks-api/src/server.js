0bb6288a13ef8f19fc2f633ed0322c825e8b715c
Pratik Athawale <pratik.athawale@sincrodigital.com>
2025-12-26 19:27:27 +0530

    dockerize notesbook api

--- notebooks-api/src/server.js
+++ notebooks-api/src/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const port = process.env.PORT;
+
 app.get('/', (req, res) => {
   res.send('Hello from Notebooks!');
 });
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
